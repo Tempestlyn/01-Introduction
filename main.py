b7e0fb432b66fd4c100ef485dbeed5470b64ce77
jwringer <jwringer@iu.edu>
2019-08-28 13:11:31 -0400

    Completed

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+complete = False
+name = "Jacob"
+game = "Total War Warhammer"
+concerns = "applying code from languages within the class to c# and c++"
+excite = "use a new engine"
+usernumber = "11990493"
+github = "https://github.com/Tempestlyn"
+print("Hello, Class! My name is " + name + ". My favorite game is " + game + ". My main concern for this class is " + concerns + ". However, I am excited to " + excite + ". My stackoverflow user number is " + usernumber + ", and my github url is " + github + ".")
+
+    
